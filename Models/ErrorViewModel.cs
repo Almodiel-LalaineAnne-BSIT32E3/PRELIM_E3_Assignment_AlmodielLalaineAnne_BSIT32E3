944f27c5ade474b1505379e956a14433b5fa70ac
Aika <Aika@LAPTOP-LUN36AVI>
2024-03-03 02:50:14 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E3_Assessment___BSIT32E3___AlmodielLalaineAnne_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
